d76d3c9beffc05301cafda25b9b6337a05f11ccb
gacetta <78240758+gacetta@users.noreply.github.com>
2022-12-26 17:31:40 -0800

    feat: worked through doubly linked list

--- zzTest.js
+++ zzTest.js
@@ -198,6 +198,26 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  unshift(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.tail = newNode;
+      // console.log('adding to empty DLL.  setting tail to new Node', this.tail)
+    } else {
+      // console.log('adding to existing DLL.')
+      const firstNode = this.head;
+      // console.log('firstNode: ', firstNode)
+      firstNode.prev = newNode;
+      // console.log('firstNode.prev: ', firstNode.prev)
+      newNode.next = firstNode;
+      // console.log('newNode.next: ', firstNode)
+    }
+    this.head = newNode;
+    // console.log('setting head to new Node', this.head)
+    this.length++;
+    // console.log('incrementing length: ', this.length)
+    return this;
+  }
   pop() {
     if (this.length === 0) return undefined;
     const removedNode = this.tail;
@@ -228,19 +248,7 @@ class DoublyLinkedList {
     this.length--;
     return removedNode
   }
-  unshift(value) {
-    const newNode = new Node(value);
-    if (this.length === 0) {
-      this.tail = newNode;
-    } else {
-      const firstNode = this.head;
-      firstNode.prev = newNode;
-      newNode.next = firstNode;
-    }
-    this.head = newNode;
-    this.length++;
-    return this;
-  }
+  
   get(index) {
     if (index < 0 || index > this.length) return null;
     let count;
@@ -310,10 +318,22 @@ class DoublyLinkedList {
   }
 }
 
-const DLL = new DoublyLinkedList();
-DLL.push(100)
-console.log(DLL.insert(1, 200));
-DLL.push(300)
-console.log(DLL)
-console.log(DLL.remove(0))
-console.log(DLL)
+// const DLL = new DoublyLinkedList();
+// console.log(DLL);
+
+
+var doublyLinkedList = new DoublyLinkedList;
+console.log(doublyLinkedList.unshift(5)); // doublyLinkedList
+console.log(doublyLinkedList.length); // 1
+console.log(doublyLinkedList.head.val); // 5
+console.log(doublyLinkedList.tail.val); // 5
+console.log(doublyLinkedList.unshift(10)); doublyLinkedList 
+console.log(doublyLinkedList.length); // 2
+console.log(doublyLinkedList.head.val); // 10
+console.log(doublyLinkedList.head.next.val); // 5
+console.log(doublyLinkedList.tail.val); // 5
+console.log(doublyLinkedList.unshift(15)); doublyLinkedList
+console.log(doublyLinkedList.length); // 3
+console.log(doublyLinkedList.head.val); // 15
+console.log(doublyLinkedList.tail.val); // 5
+console.log(doublyLinkedList.head.next.next.val); // 5
