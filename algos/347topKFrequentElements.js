79e44d40664cdd55603e68769e427b1e41961d03
gacetta <78240758+gacetta@users.noreply.github.com>
2023-06-02 10:33:57 -0700

    add: topKFreq with bucket sort

---
+++ algos/347topKFrequentElements.js
@@ -0,0 +1,52 @@
+/*
+Given an integer array nums and an integer k, return the k most frequent elements. 
+You may return the answer in any order.
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+ 
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+*/
+
+function topKFrequent(nums, k) {
+  const freqMap = new Map();
+  const bucket = [];
+  const output = [];
+
+  // build cache
+  nums.forEach((number) => {
+    freqMap.set(number, (freqMap.get(number) || 0) + 1)
+  })
+
+  // build bucket
+  for (let [num, freq] of freqMap) {
+    bucket[freq] = (bucket[freq] || new Set()).add(num)
+  }
+
+  // build ouputArr
+  for (let i = bucket.length - 1; i >= 0; i--) {
+    if (bucket[i]) output.push(...bucket[i]);
+    if (output.length === k) break;
+  }
+
+  return output;
+}
+
+const test1 = [1, 1, 1, 2, 2, 3];
+const test2 = [1, 1, 2]
+
+console.log(topKFrequent(test1, 2));
+console.log(topKFrequent(test2, 1));
