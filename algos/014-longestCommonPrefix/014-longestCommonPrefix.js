625a80987bc1c1036035eae30fa6713379fe560d
gacetta <78240758+gacetta@users.noreply.github.com>
2024-05-03 17:05:07 -0700

    update: longestCommonPrefix js solution

--- algos/014-longestCommonPrefix/014-longestCommonPrefix.js
+++ algos/014-longestCommonPrefix/014-longestCommonPrefix.js
@@ -3,19 +3,21 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    prefix = '';
-    if (strs.length === 0) return prefix;
+    let prefix = ''; // Initialize the common prefix string
 
-    // loop through characters
-    for (let i = 0; i < strs[0].length; i++) {
-        // loop through strings (str)
-        for (let j = 0; j < strs.length; j++) {
-            if (strs[j][i] !== strs[0][i]) {
-                return prefix;
+    // Loop through each character of the first string in strs
+    for (let charIndex = 0; charIndex < strs[0].length; charIndex++) {
+        const currentCharacter = strs[0][charIndex]; // Get the current character being checked
+        // Loop through each string in strs starting from the second string
+        for (let stringIndex = 0; stringIndex < strs.length; stringIndex++) {
+            const currentString = strs[stringIndex]; // Get the current string being checked
+            // Check if the character at the same index in the current string is different
+            // from the character in the same position in the first string
+            if (currentString[charIndex] !== currentCharacter) {
+                return prefix; // If different, return the current common prefix
             }
         }
-        prefix += strs[0][i];
+        prefix += currentCharacter; // If all strings have the same character at the current index, add it to the prefix
     }
-    return prefix;
-
+    return prefix; // Return the longest common prefix found
 };
